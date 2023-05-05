b9e6660072d6b0036dbb2bf2985a907333adaab8
giorgisilagadze <silagadze.gio123@gmail.com>
2023-05-05 17:50:18 +0400

    feat: contact with API is done

--- src/components/Advice.jsx
+++ src/components/Advice.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import {
   Container,
   AdviceId,
@@ -5,12 +6,25 @@ import {
   Divider,
   DiceButt,
 } from "../styled-components/Advice.Styled";
+import axios from "axios";
 
 export default function Advice() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const getApi = (async () => {
+      const response = await axios.get("https://api.adviceslip.com/advice");
+      const info = response.data;
+
+      setData(info);
+      console.log(info);
+    })();
+  }, []);
+
   return (
     <>
       <Container>
-        <AdviceId></AdviceId>
+        <AdviceId>ADVICE #{data?.slip.id}</AdviceId>
         <AdviceTxt></AdviceTxt>
         <Divider src="./images/pattern-divider-mobile.svg" />
       </Container>
